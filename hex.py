b8f1206f666ffb2cb747750fe4eb2ae2473151f1
Jamie Woodward <158477225+ZSH4RK@users.noreply.github.com>
2026-03-04 13:37:25 +0000

    Rewriting Hex Class with coordinates

--- hex.py
+++ hex.py
@@ -1,4 +1,7 @@
+from typing import Tuple
+
 class Hex:
+
     def __init__(self, x, y, z, value=0, base=None):
         if x + y + z != 0:
             raise ValueError("x + y + z must equal 0")
@@ -7,3 +10,12 @@ class Hex:
         self.z = z
         self.value = value
         self.base = base if base is not None else value
+        # Strart from the most left then clockwise
+        self.neighbour_directions = [   (-1, 0, 1), (0, -1, 1),
+                                        (1, -1, 0), (1, 0, -1),
+                                        (0, 1, -1), (-1, 1, 0)]
+
+    def get_neighbour(self, direction: Tuple[int, int, int]):
+        return Hex(direction)
+
+
