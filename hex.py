b2cc28f79f14779aad627e5f17d2bc850de364ee
Jamie Woodward <158477225+ZSH4RK@users.noreply.github.com>
2026-03-05 13:37:02 +0000

    Added Hex Distances

--- hex.py
+++ hex.py
@@ -17,5 +17,28 @@ class Hex:
 
     def get_neighbour(self, direction: Tuple[int, int, int]):
         return Hex(direction)
+    
+    def cube_to_axial(self):
+        q = self.x
+        r = self.r
+        return (q,r)
+    
+    def axial_to_cube(self, a_coords: Tuple[int, int]):
+        q, r = a_cords
+        x = q
+        y = r
+        z = -q-r
+        return (x, y, z)
+    
+
+
+# Calculate distances
+def cube_subtract(a: Hex, b: Hex):
+    return (a.x-b.x, a.y-b.y, a.z-b.z)
+
+def cube_distance(a, b):
+    vec = cube_subtract(a, b)
+    return (abs(vec[1]), abs(vec[2]), abs(vec[3])) / 2
+    
 
 
